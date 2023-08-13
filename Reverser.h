4c4be080001f81cd4c0f102c2da09810125b3599
a1810246 <71242560+a1810246@users.noreply.github.com>
2023-08-13 13:57:53 +0000

    test

---
+++ Reverser.h
@@ -0,0 +1,12 @@
+#ifndef REVERSER_H
+#define REVERSER_H
+#include<string>
+
+class Reverser{
+    public:
+        Reverser();
+        int reverseDigit(int value);
+        string reverseString(string characters);
+}
+
+#endif
