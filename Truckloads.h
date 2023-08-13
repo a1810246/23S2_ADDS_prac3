044bf3c37f325e9fee2d5f2acbe25f40e1edd53b
a1810246 <71242560+a1810246@users.noreply.github.com>
2023-08-13 13:58:03 +0000

    test

---
+++ Truckloads.h
@@ -0,0 +1,14 @@
+#ifndef TRUCKLOADS_H
+#define TRUCKLOADS_H
+#include string
+
+class Truckloads{
+    public:
+        Truckloads();
+        int numTrucks(int numCrates, int loadSize);
+
+
+}
+
+
+#endif
