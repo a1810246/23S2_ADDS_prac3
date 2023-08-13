7716ad76b5fe29fff43429d0e1cc571a1d5a0bfa
a1810246 <71242560+a1810246@users.noreply.github.com>
2023-08-13 14:24:26 +0000

    test

--- Truckloads.cpp
+++ Truckloads.cpp
@@ -6,8 +6,14 @@ Truckloads::Truckloads()
 }
 
 int Truckloads::numTrucks(int numCrates, int loadSize){
-    if(numCrates<=loadSize)return 1;
+    if(numCrates<=loadSize)
+    {
+        return 1;
+    }
 
-    else return numTrucks(numCrates/2,loadSize)+numTrucks((numCrates+1)/2,loadSize);
+    else
+    {
+        return numTrucks(numCrates/2,loadSize)+numTrucks((numCrates+1)/2,loadSize);
+    }
 }
 
